4373ad0d0115b03b80efd912f481df23fc958045
CaoRuis <1575825411@qq.com>
2022-08-19 15:48:04 +0800

    style ：新增尺寸适配

---
+++ app/src/main/java/com/test/rebuildintroduction/fragment/TypeFragment.kt
@@ -0,0 +1,13 @@
+package com.test.rebuildintroduction.fragment
+
+import com.test.rebuildintroduction.base.BaseFragment
+
+class TypeFragment : BaseFragment() {
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initData() {
+        TODO("Not yet implemented")
+    }
+}
